42cc3d0dc705b2e38b2894341cb24f343dcbfe81
DalgonaFox <mila.olisantos@gmail.com>
2025-03-01 20:08:32 -0300

    adicionei email com muita dificuldade

--- server.js
+++ server.js
@@ -1,81 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const track = document.querySelector(".card-track");
-    const prevBtn = document.querySelector(".prev");
-    const nextBtn = document.querySelector(".next");
-
-    let index = 0;
-    const totalCards = document.querySelectorAll(".card-track .card").length;
-    const cardsPerView = 2;
-    const maxIndex = Math.ceil(totalCards / cardsPerView);
-
-    function updateCarousel() {
-        const moveAmount = index * -33.33;
-        track.style.transform = `translateX(${moveAmount}%)`;
-    }
-
-    nextBtn.addEventListener("click", function () {
-        if (index < maxIndex) {
-            index++;
-            updateCarousel();
-        }
-    });
-
-    prevBtn.addEventListener("click", function () {
-        if (index > 0) {
-            index--;
-            updateCarousel();
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const track = document.querySelector(".card-track2");
-    const prevBtn = document.querySelector(".prev2");
-    const nextBtn = document.querySelector(".next2");
-
-    let index = 0;
-    const totalCards = document.querySelectorAll(".card-track2 .card").length;
-    const cardsPerView = 2;
-    const maxIndex = Math.ceil(totalCards / cardsPerView);
-
-    function updateCarousel() {
-        const moveAmount = index * -33.33;
-        track.style.transform = `translateX(${moveAmount}%)`;
-    }
-
-    nextBtn.addEventListener("click", function () {
-        if (index < maxIndex) {
-            index++;
-            updateCarousel();
-        }
-    });
-
-    prevBtn.addEventListener("click", function () {
-        if (index > 0) {
-            index--;
-            updateCarousel();
-        }
-    });
-});
-
+require("dotenv").config();
 var express = require('express');
-var session = require('express-session');
-var app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require("nodemailer");
+const cors = require("cors"); 
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "OK" : "NÃO DEFINIDO");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
 
 app.use(express.static(__dirname + '/public'));
 
-//app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {sendMessage: null});
 });
 
 app.get("/English", (req, res) => {
     res.render("english");
 });
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+  });
+
+  app.post("/send-email", async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    const mailOptions = {
+        from: `"${name}" <${process.env.EMAIL_USER}>`,
+        replyTo: email,
+        to: process.env.EMAIL_DEST,
+        subject: `${subject}`,
+        text: `E-mail do Remetente: ${email}\n ${message}`
+    };
+
+    if (error){
+        console.error("Erro ao enviar e-mail:", error);
+    } else  {
+        console.log("Mensagem enviada com sucesso!");
+        return res.render('index', {sendMessage: 'E-mail enviado com sucesso!' });
+    }
+});
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
